4beaea35fe4798599c7ee46627425d8e9414e465
eastoni123 <onkysoerya@gmail.com>
2018-10-30 22:35:10 +0700

    add-title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Rosette</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="rosette,technology,cloud">
